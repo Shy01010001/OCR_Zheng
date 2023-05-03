73f9e0f224dc2f19867092957f1baf116817934d
You <youjingyi123@outlook.com>
2023-05-03 10:32:01 +0800

    读Gt.txt

---
+++ ReadGtTxt.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 08:49:01 2023
+
+@author: youjingyi
+"""
+for line in open('./output/gt.txt', encoding = 'shift-jis'):
+    print(line)
+
+# 创建一个用于保存新闻文本文件的目录
