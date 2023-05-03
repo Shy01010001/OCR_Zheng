5d5c04de7ad810898c1ca47bcee945ac37917b6f
You <youjingyi123@outlook.com>
2023-05-03 11:02:13 +0800

    new data

--- GenerateGt.py
+++ GenerateGt.py
@@ -4,7 +4,7 @@ Created on Wed May  3 08:49:49 2023
 
 @author: youjingyi
 """
-
+import re
 from trdg.generators import(
     GeneratorFromDict,
     GeneratorFromRandom,
@@ -14,23 +14,25 @@ from trdg.generators import(
 	# The generators use the same arguments as the CLI, only as parametersgenerator=GeneratorFromStrings(
 imgNum = 0
 with open('./output/gt.txt', 'a', encoding = 'shift-jis') as f:
-    for line in open('./data/GT/result.txt', encoding = 'shift-jis'):
-        imgN = str(imgNum).zfill(8)
-        t = f'images/{imgN}.png    ' + line
-        generator = GeneratorFromStrings(
-        [line],
-        # blur=2,
-        random_blur=False,
-        language = 'ja'
-        )
-        try:
-            
-            for img, lbl in generator:
-            # print(generator[0])
-                img.save(f'./output/images/{imgN}.png')
-                break
-            f.write(t)
-            imgNum += 1
-        except:
-            print(line,' cannot generate image!')
+    for sents in open('./data/GT/result.txt', encoding = 'shift-jis'):
+        for line in re.findall(r'.{10}', sents):
+            line = line + '\n'
+            imgN = str(imgNum).zfill(8)
+            t = f'images/{imgN}.png    ' + line
+            generator = GeneratorFromStrings(
+            [line],
+            # blur=2,
+            random_blur=False,
+            language = 'ja'
+            )
+            try:
                 
+                for img, lbl in generator:
+                # print(generator[0])
+                    img.save(f'./output/images/{imgN}.png')
+                    break
+                f.write(t)
+                imgNum += 1
+            except:
+                print(line,' cannot generate image!')
+                    
