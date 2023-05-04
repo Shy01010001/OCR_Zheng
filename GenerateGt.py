9b4bd9f42166c1d64924764ad4389017d04de020
You <youjingyi123@outlook.com>
2023-05-04 19:52:41 +0800

    由于re.findall（s‘.{10}’, sents）当sents长度小于10，会返回空字符串，导致没有ground truth 因此过滤掉长度小于10的行

--- GenerateGt.py
+++ GenerateGt.py
@@ -5,6 +5,8 @@ Created on Wed May  3 08:49:49 2023
 @author: youjingyi
 """
 import re
+import shutil
+
 from trdg.generators import(
     GeneratorFromDict,
     GeneratorFromRandom,
@@ -13,26 +15,31 @@ from trdg.generators import(
 )
 	# The generators use the same arguments as the CLI, only as parametersgenerator=GeneratorFromStrings(
 imgNum = 0
+# shutil.rmtree('./output/images/')
 with open('./output/gt.txt', 'a', encoding = 'shift-jis') as f:
     for sents in open('./data/GT/result.txt', encoding = 'shift-jis'):
-        for line in re.findall(r'.{10}', sents):
-            line = line + '\n'
-            imgN = str(imgNum).zfill(8)
-            t = f'images/{imgN}.png\t' + line
-            generator = GeneratorFromStrings(
-            [line],
-            # blur=2,
-            random_blur=False,
-            language = 'ja'
-            )
-            try:
-                
-                for img, lbl in generator:
-                # print(generator[0])
-                    img.save(f'./output/images/{imgN}.png')
-                    break
-                f.write(t)
-                imgNum += 1
-            except:
-                print(line,' cannot generate image!')
+        # print(len(sents))
+        if len(sents) > 10:
+            for line in re.findall(r'.{10}', sents):
+                line = line + '\n'
+                imgN = str(imgNum).zfill(8)
+                t = f'images/{imgN}.png\t' + line
+                generator = GeneratorFromStrings(
+                [line],
+                # blur=2,
+                random_blur=False,
+                language = 'ja'
+                )
+                try:
                     
+                    for img, lbl in generator:
+                    # print(generator[0])
+                        img.save(f'./output/images/{imgN}.png')
+                        break
+                    f.write(t)
+                    imgNum += 1
+                except:
+                    print(line,' cannot generate image!')
+                        
+        else:
+            continue
--- ReadGtTxt.py
+++ ReadGtTxt.py
@@ -4,7 +4,8 @@ Created on Mon May  1 08:49:01 2023
 
 @author: youjingyi
 """
+a = []
 for line in open('./output/gt.txt', encoding = 'shift-jis'):
     print(line)
-
+    a.append(line)
 # 创建一个用于保存新闻文本文件的目录
